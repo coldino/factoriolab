050bf4ef4143f259518c452f726118203543f42c
coldino <28345893+coldino@users.noreply.github.com>
2026-01-12 14:25:54 +0000

    LIVE: Change default game to StarRupture for now

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,7 +1,7 @@
 import { Entities } from './utils';
 
-export const APP = 'FactorioLab';
-export const DEFAULT_MOD = 'spa';
+export const APP = 'SRLab';
+export const DEFAULT_MOD = 'str';
 export const MIN_LINK_VALUE = 1e-10;
 export const MIN_ZIP = 200;
 export const ZEMPTY = '_';
